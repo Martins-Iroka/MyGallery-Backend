dbd256f0f6cdcfbe740794118e3540b9ead3db6d
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-11-08 22:54:18 +0100

    feat: implement API call to fetch curated photos from Pexels

--- cmd/migrate/seed/main.go
+++ cmd/migrate/seed/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Martins-Iroka/MyGallery-Backend/internal/env"
+)
+
 type PexelsResponse struct {
 	Photos []Photo `json:"photos"`
 }
@@ -14,5 +24,49 @@ type Photo struct {
 }
 
 func main() {
+	PEXELS_API_URL := "https://api.pexels.com/v1/curated"
 
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	req, err := http.NewRequest("GET", PEXELS_API_URL, nil)
+	if err != nil {
+		log.Printf("Error creating request: %v", err)
+		return
+	}
+
+	req.Header.Add("Authorization", env.GetString("PEXELS_API_KEY", ""))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error executing request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("API request failed with status: %s", resp.Status)
+		return
+	}
+
+	// Read the body
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %v", err)
+		return
+	}
+
+	// Unmarshal JSON into the struct
+	var pexelsData PexelsResponse
+	err = json.Unmarshal(bodyBytes, &pexelsData)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+
+	// Successfully parsed the data!
+	log.Printf("Job Success! Fetched %d.", len(pexelsData.Photos))
+
+	// Example of using the data:
+	if len(pexelsData.Photos) > 0 {
+		log.Printf("First Photo: ID %d by %s", pexelsData.Photos[0].ID, pexelsData.Photos[0].Photographer)
+	}
 }
