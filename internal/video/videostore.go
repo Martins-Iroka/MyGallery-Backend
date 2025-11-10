36c0fe931d40e1d7290e9fe4dd5938b919a07b82
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-11-10 16:14:46 +0100

    feat: enhance video storage functionality with JSON handling for download files

--- internal/video/videostore.go
+++ internal/video/videostore.go
@@ -3,6 +3,7 @@ package video
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 
 	"github.com/Martins-Iroka/MyGallery-Backend/internal/util"
 )
@@ -19,10 +20,10 @@ type VideoDownloadFile struct {
 	Video_Link    string
 	Video_Size    int32
 }
-
+type VideoDownloadFileList []VideoDownloadFile
 type VideoPostAndDownloadFile struct {
 	VideoPost
-	files []VideoDownloadFile
+	files VideoDownloadFileList
 }
 
 type VideoStore struct {
@@ -30,16 +31,88 @@ type VideoStore struct {
 }
 
 func (v *VideoStore) CreateVideoPost(ctx context.Context, videoPost *VideoPost) error {
+	query := `
+		INSERT INTO video_posts (id, video_url, duration) VALUES ($1, $2, $3)
+	`
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
 
+	_, err := v.Db.ExecContext(ctx, query, videoPost.ID, videoPost.Video_Url, videoPost.Duration)
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (v *VideoStore) CreateVideoDownloadFile(ctx context.Context, file *VideoDownloadFile, videoPostId int64) error {
+	query := `
+		INSERT INTO video_download_files (id, video_post_id, video_link, video_size) VALUES ($1, $2, $3, $4)
+	`
 
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := v.Db.ExecContext(ctx, query, file.ID, videoPostId, file.Video_Link, file.Video_Size)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (v *VideoStore) GetVideoPostAndDownloadFile(ctx context.Context, paginate util.PaginatedPostQuery) ([]VideoPostAndDownloadFile, error) {
+	query := `
+		SELECT 
+			vp.id,
+			vp.video_url,
+			vp.duration,
+			COALESCE(
+				json_agg(
+					json_build_object(
+						'id', vdf.id,
+						'video_post_id', vdf.video_post_id,
+						'video_link', vdf.video_link,
+						'video_size', vdf.video_size
+					)
+				) FILTER (WHERE vdf.id IS NOT NULL),
+				'[]'
+			) as download_files
+		FROM video_posts vp
+		LEFT JOIN video_download_files vdf ON vp.id = vdf.video_post_id
+		GROUP BY vp.id, vp.video_url, vp.duration
+		ORDER BY vp.id DESC
+		LIMIT $1 OFFSET $2
+	`
 
-	return nil, nil
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
+
+	rows, err := v.Db.QueryContext(ctx, query, paginate.Limit, paginate.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []VideoPostAndDownloadFile
+	for rows.Next() {
+		var vp VideoPostAndDownloadFile
+		var filesJSON []byte
+
+		if err := rows.Scan(&vp.ID, &vp.Video_Url, &vp.Duration, &filesJSON); err != nil {
+			return nil, err
+		}
+
+		// Unmarshal the JSON array of download files
+		if err := json.Unmarshal(filesJSON, &vp.files); err != nil {
+			return nil, err
+		}
+
+		results = append(results, vp)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }
