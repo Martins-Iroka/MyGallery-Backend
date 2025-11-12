5b2831d455aca12a4708c549d4c19e47b1e80b16
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-11-12 16:04:17 +0100

    feat: add CORS middleware to API for enhanced security and cross-origin requests

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -10,8 +10,10 @@ import (
 	"github.com/Martins-Iroka/MyGallery-Backend/docs"
 	"github.com/Martins-Iroka/MyGallery-Backend/internal"
 	"github.com/Martins-Iroka/MyGallery-Backend/internal/auth"
+	"github.com/Martins-Iroka/MyGallery-Backend/internal/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 	"go.uber.org/zap"
 )
@@ -31,6 +33,14 @@ func (app *application) Mount() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{env.GetString("CORS_ALLOWED_ORIGIN", "http://127.0.0.1:4040")},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/go-chi/cors v1.2.2
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.28.0
