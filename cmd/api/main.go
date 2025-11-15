75e7b228cdbb083ab81d23c873973f4b78b9c6c0
GitHub Action <action@github.com>
2025-11-15 15:52:52 +0000

    Update version to 1.4.0

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,7 +11,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const version = "1.3.0"
+const version = "1.4.0"
 
 //	@title			MiGalaria API
 //	@description	API for MiGalaria, an application for pictures and short videos.
